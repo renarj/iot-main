1eed6662a85eba2c8a06dbc8533b3b30dde92e8b
renarj <renze@renarj.nl>
2022-08-07 10:50:27 +0200

    Renamed iot-main directory

---
+++ iot-base/home-service/src/main/java/com/oberasoftware/home/service/events/controller/DeviceUpdateHandler.java
@@ -0,0 +1,27 @@
+package com.oberasoftware.home.service.events.controller;
+
+import com.oberasoftware.base.event.EventHandler;
+import com.oberasoftware.base.event.EventSubscribe;
+import com.oberasoftware.iot.core.events.DeviceUpdateEvent;
+import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import static org.slf4j.LoggerFactory.getLogger;
+
+/**
+ * @author renarj
+ */
+@Component
+public class DeviceUpdateHandler implements EventHandler {
+    private static final Logger LOG = getLogger(DeviceUpdateHandler.class);
+
+    @Autowired
+    private DeviceManager deviceManager;
+
+    @EventSubscribe
+    public void receive(DeviceUpdateEvent event) throws Exception {
+        LOG.debug("Received a device update for plugin: {} and device: {}", event.getPluginId(), event.getDevice());
+        deviceManager.registerThing(event.getDevice());
+    }
+}
